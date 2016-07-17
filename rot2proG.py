e89c53a7baf9c15c1e966285bfb7fd766fcd260a
Jaiden Ferraccioli <jaidenfe@buffalo.edu>
2016-07-16 22:44:15 -0400

    Completed Software Interface Documentation

--- rot2proG.py
+++ rot2proG.py
@@ -1,6 +1,3 @@
-import serial
-import time
-
 '''
 File: 	rot2proG.py
 Author: Jaiden Ferraccioli
@@ -11,11 +8,33 @@ Brief: 	This class is a control interface for the SPID Elektronik rot2proG anten
 	and a moving target. 
 '''
 
+import serial
+import time
+
+'''
+This class defines the control interface for the SPID Elektronik rot2proG antenna rotor controller.
+
+Note: 	The controller will not change azimuth or elevation values unless the rotor is connected.
+
+Setup:	The controller must be set to use the "SPID" protocol. To do this, press the 'S' button on the
+	controller until it says 'PS' along with the current azimuth and elevation. Then the left or
+	right buttons on the controller to change between protocols. Select the protocol saying 'SP' in
+	the 'Horizontal' field of the controller. Once the controller is set to use the SPID protocol,
+	we must put it into automated mode. To do this, press the 'F' button until the controller reads
+	'A' along with the current azimuth and elevation. You are now ready to communicate with the
+	rotor controller.
+'''
 class Rot2proG:
 
 	pulse = 0
 	debug = False
 
+	'''
+	This sets up the serial connection and pulse value.
+	When set to true, the debugging parameter allows for information such as
+	asimuth, elevation and pulse to be printed out when functions are called.
+	Debugging defualts to False.
+	'''
 	def __init__(self, debugging=False):
 		self.ser = serial.Serial(port='/dev/ttyUSB0',baudrate=600, bytesize=8, parity='N', stopbits=1, timeout=None)
 		self.status()
@@ -24,9 +43,19 @@ class Rot2proG:
 			print(self.ser.name)
 			print("Pulse: " + str(self.pulse) + "\n")
 
+	'''
+	This makes sure that the serial connection is closed when the class is deleted 
+	or the program terminates
+	'''
 	def __del__(self):
 		self.ser.close()
 
+	'''
+	Send a STATUS command to the controller, which requests the current azimuth
+	and elevation of the rotor. The azimuth, elevation and pulse are then computed,
+	the pulse is set and the azimuth, elevation and pulse are returned as a list (first
+	element being azimuth, the second being elevation, and the third being pulse).
+	'''
 	def status(self):
 		cmd = ['\x57','\x00','\x00','\x00','\x00','\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x1f', '\x20']
 		packet = "".join(cmd)
@@ -41,6 +70,8 @@ class Rot2proG:
 		ph = ord(rec_packet[5])
 		pv = ord(rec_packet[10])
 
+		ret = [az, el, ph]
+
 		assert(ph == pv)
 		self.pulse = ph
 
@@ -51,6 +82,15 @@ class Rot2proG:
 			print("PH: " + str(ph))
 			print("PV: " + str(pv) + "\n")
 
+		return ret
+
+	'''
+	Send a STOP command to the controller, which causes the rotor to stop moving and
+	return the current azimuth, elevation and pulse of the rotor where it stopped. The
+	azimuth, elevation and pulse are then computed, the pulse is set and the azimuth,
+	elevation and pulse are returned as a list (first element being azimuth, sencond
+	being elevation and the third being pulse).
+	'''
 	def stop(self):
 		cmd = ['\x57','\x00','\x00','\x00','\x00','\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x0f', '\x20']
 		packet = "".join(cmd)
@@ -65,6 +105,8 @@ class Rot2proG:
 		ph = ord(rec_packet[5])
                 pv = ord(rec_packet[10])
 
+		ret = [az, el, ph]
+
 		assert(ph == pv)
                 self.pulse = ph
 
@@ -75,6 +117,14 @@ class Rot2proG:
                         print("PH: " + str(ph))
                         print("PV: " + str(pv) + "\n")
 
+		return ret
+
+	'''
+	send a SET command to the controller, which causes the rotor to adjust its position
+	to the azimuth and elevation specified by the azi and eli parameters respecitvely.
+	The azi and eli parameters are floating point values that specify the desired position.
+	There is no response to the SET command, thus nothing to return.
+	'''
 	def set(self, azi, eli):
 		az = "0" + str((self.pulse * (azi + 360)))
 		el = "0" + str((self.pulse * (eli + 360)))
@@ -94,6 +144,10 @@ class Rot2proG:
 
 		time.sleep(1)
 
+	'''
+	Calls the STATUS, STOP and SET functions multiple times
+	in order to test the rot2proG class functionality
+	'''
 	def test(self):
 		rot = Rot2proG(True)
 		rot.status()
